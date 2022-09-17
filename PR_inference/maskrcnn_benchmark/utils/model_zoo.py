27bf8b946460d2557e6c4a953a9012ca811fddcb
jbyuki <jyburkhard@hotmail.com>
2022-09-17 12:17:42 +0200

    fix compat

--- PR_inference/maskrcnn_benchmark/utils/model_zoo.py
+++ PR_inference/maskrcnn_benchmark/utils/model_zoo.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 from torch.hub import download_url_to_file
-from torch.utils.model_zoo import urlparse
-from torch.utils.model_zoo import HASH_REGEX
+from torch.hub import urlparse
+from torch.hub import HASH_REGEX
 
 from maskrcnn_benchmark.utils.comm import is_main_process
 from maskrcnn_benchmark.utils.comm import synchronize
